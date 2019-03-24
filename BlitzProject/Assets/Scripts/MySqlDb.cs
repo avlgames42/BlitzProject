b9ebdee699921605243cddbf2eaf1142488f5c60
unknown <larissastorck@hotmail.com>
2019-03-24 18:41:29 -0300

    Salva os dados do player e knn data

--- BlitzProject/Assets/Scripts/MySqlDb.cs
+++ BlitzProject/Assets/Scripts/MySqlDb.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using MySql.Data.MySqlClient;
 using System.Data;
+using System;
 
 public class MySqlDb : MonoBehaviour
 
 {
     string urlPost = "http://ec2-54-69-94-4.us-west-2.compute.amazonaws.com/InsertPlayerInfo.php";
+    string urlPostKnn = "http://ec2-54-69-94-4.us-west-2.compute.amazonaws.com/InsertKnnData.php";
 
-     public void Start()
+    public int cdPlayer;
+    
+    
+
+
+    public void Start()
     {
+
     }
 
 
@@ -20,13 +28,18 @@ public class MySqlDb : MonoBehaviour
         ResearchControl researchScript = research.GetComponent<ResearchControl>();
 
         InsertPlayerInfo(researchScript.jogador);
+   
     }
 
 
     public void InsertPlayerInfo(string[] jogador)
     {
-        print("InsertPlayerInfo");
+
         int playGames;
+
+        GameObject user = GameObject.Find("GetUser");
+        CdPlayer getUser = user.GetComponent<CdPlayer>();
+        cdPlayer = getUser.cdPlayer;
 
 
 
@@ -39,8 +52,10 @@ public class MySqlDb : MonoBehaviour
             playGames = 0;
         }
 
+        print("PlayerInfo cdPlayer" + cdPlayer);
 
         WWWForm form = new WWWForm();
+        form.AddField("cdPlayerPost", cdPlayer);
         form.AddField("playerNamePost", jogador[0]);
         form.AddField("genderPost", jogador[2]);
         form.AddField("agePost", jogador[1]);
@@ -48,7 +63,44 @@ public class MySqlDb : MonoBehaviour
         form.AddField("cityPost", jogador[4]);
         form.AddField("playGamesPost", playGames);
 
-        WWW www = new WWW(urlPost, form );
-
+        WWW www = new WWW(urlPost, form);
+   
     }
+
+
+    public void InsertKnnData(float[] knn, float distanceAux)
+    {
+        GameObject user = GameObject.Find("GetUser");
+        CdPlayer getUser = user.GetComponent<CdPlayer>();
+        cdPlayer = getUser.cdPlayer;
+
+
+
+
+        print("KnncdPlayer" + cdPlayer);
+
+        WWWForm form = new WWWForm();
+        form.AddField("cdplayerPost", cdPlayer);
+        form.AddField("intOfShootsPost", (knn[0]).ToString());
+        form.AddField("intOfHitsPost", (knn[1]).ToString());
+        form.AddField("intOfBoxesPost", (knn[2]).ToString());
+        form.AddField("hpLostPost", (knn[3]).ToString());
+        form.AddField("healPost", (knn[4]).ToString());
+        form.AddField("secondsPost", (knn[5]).ToString());
+        form.AddField("distancePost", 0);
+        form.AddField("firstSkillPost", (knn[9]).ToString());
+        form.AddField("mostUsedSkillPost", 0);
+        form.AddField("distanceOfEnemysPost", (knn[6]).ToString());
+        form.AddField("distanceInRoomPost", 0);
+        form.AddField("distanceAuxPost", (distanceAux).ToString());
+
+        WWW www = new WWW(urlPostKnn, form);
+
+        
+    }
+
+
+
+
+
 }
